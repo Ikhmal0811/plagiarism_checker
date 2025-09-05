515d7a8673c7ec791ee773117b7f4330727a703d
Ikhmal <mhd1ikhmal@gmail.com>
2025-09-05 14:34:42 +0800

    still in phase 1

--- wordle.py
+++ wordle.py
@@ -1,2 +1,20 @@
-f = open("write.txt", "r")
-printf(f.read())
+import random
+
+f = open("word.txt", "r")
+
+#def checkWords(theWords):
+
+
+words = f.read().splitlines()
+countWords = len(words)
+
+randomIndex = random.choice(words)
+
+print(randomIndex)
+
+#i = 0
+#while(i < countWords):
+#    splitwords = words[i].split()
+#    checkWords(splitwords)
+#    print(words[i])
+#    i+=1
